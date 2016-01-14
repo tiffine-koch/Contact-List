3b5c0425c8a06ec6a57056fe2a69c982ea1977cd
Apple <tiffine.koch@gmail.com>
2016-01-13 22:33:59 -0800

    added local storage functions

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 $(document).ready(init);
 
+var contactList = [];
+var contact = {};
+
 function init() {
+  loadFromStorage();
+  updateList();
 
   $('.dropList').on('click', findCat)
   $('#formEntry').submit(newEntry);
   $('#catDropDown').on('change', findCat);
   $('table').on('click', '#trash', deleteTrans);
-  //sort on click indiv headers
-  // console.log('load');
 }
 
 function removeEntries() {
@@ -17,45 +20,45 @@ function removeEntries() {
 function newEntry() {
   event.preventDefault();
 
-  var conName = $('#conEntry').val();
-  var email = $('#emailEntry').val();
-  var loc = $('#locEntry').val();
-  var phone = $('#phoneEntry').val();
-  var face = $('#faceEntry').val();
-
+  contact.name = $('#conEntry').val();
+  contact.email = $('#emailEntry').val();
+  contact.loc = $('#locEntry').val();
+  contact.phone = $('#phoneEntry').val();
+  contact.face = $('#faceEntry').val();
+  contactList.push(contact);
 
   var $tr = $('#template').clone();
-  $tr.removeAttr('id');
-  $tr.children('.name').text(conName);
-  $tr.children('.email').text(email);
-  $tr.children('.location').text(loc);
-  $tr.children('.phone').text(phone);
-  $tr.children('.fb').text(face);
-  // adding switch operator
-  // switch (catEntry) {
-  //   case 'Bill':
-  //     $newAmount.addClass('testWith');
-  //     $newAmount.text('-' + Math.abs(amountEntry));
-  //     break
-  //   case 'Income':
-  //     $newAmount.addClass('testDep');
-  //     $newAmount.text('+' + Math.abs(amountEntry));
-  //     break
-  //   case 'Entertainment':
-  //     $newAmount.addClass('testWith');
-  //     $newAmount.text('-' + Math.abs(amountEntry));
-  //     break
-  //   case 'Savings':
-  //     $newAmount.addClass('testWith');
-  //     $newAmount.text('-' + Math.abs(amountEntry));
-  //     break
-  //   default:
-  //     $newAmount.text('');
-  //   // console.log(amountEntry);
-  //   }
-  // debugger;
-  $('#list').prepend($tr);
-}
+   $tr.removeAttr('id');
+   $tr.children('.name').text(contact.name);
+   $tr.children('.email').text(contact.email);
+   $tr.children('.location').text(contact.loc);
+   $tr.children('.phone').text(contact.phone);
+   $tr.children('.fb').text(contact.face);
+   // adding switch operator
+   // switch (catEntry) {
+   //   case 'Bill':
+   //     $newAmount.addClass('testWith');
+   //     $newAmount.text('-' + Math.abs(amountEntry));
+   //     break
+   //   case 'Income':
+   //     $newAmount.addClass('testDep');
+   //     $newAmount.text('+' + Math.abs(amountEntry));
+   //     break
+   //   case 'Entertainment':
+   //     $newAmount.addClass('testWith');
+   //     $newAmount.text('-' + Math.abs(amountEntry));
+   //     break
+   //   case 'Savings':
+   //     $newAmount.addClass('testWith');
+   //     $newAmount.text('-' + Math.abs(amountEntry));
+   //     break
+   //   default:
+   //     $newAmount.text('');
+   //   // console.log(amountEntry);
+   //   }
+
+   $('#list').prepend($tr);
+ }
 
 function deleteTrans() {
   $(this).closest('tr').remove();
@@ -67,10 +70,35 @@ function findCat(event) {
   console.log(text);
 }
 
-var contact {
-  this.name = name;
-  this.email = email;
-  this.location = location;
-  this.phone = phone;
-  this.fb = fb;
+function addContact() {
+  var contact = $('#formEntry').val()
+  contactList.push(contact);
+
+  saveToStorage();
+  updateList();
+}
+
+function saveToStorage() {
+  localStorage.contacts = JSON.stringify(contacts);
+}
+
+function loadFromStorage() {
+  if(!localStorage.contacts) {
+    localStorage.contacts = '[]';
+  }
+  contacts = JSON.parse(localStorage.contacts);
+}
+
+function updateList() {
+  var $contactList = $('#contactList');
+  $contactList.empty();
+  var $contacts = contacts.map(function(contact, index) {
+    $tr.append($('<td>').text(contact.name))
+    $tr.append($('<td>').text(contact.email))
+    $tr.append($('<td>').text(contact.loc))
+    $tr.append($('<td>').text(contact.phone))
+    $tr.append($('<td>').text(contact.face))
+    return $tr
+    });
+  $contactList.append($contacts);
 }
